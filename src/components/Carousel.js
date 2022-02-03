8d22f79702250ef4f070ebef2146b647ba58a1c2
TimLaxton01 <tlaxton@twigeducatio.com>
2022-02-03 09:37:42 +0000

    added Carousel framer motion

---
+++ src/components/Carousel.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {motion} from 'framer-motion';
+import {useRef, useEffect, useState} from 'react'
+
+const Carousel = () => {
+  return (
+  <motion.h1 animate={{x: 250}}>POP</motion.h1>
+  )
+};
+
+export default Carousel;
