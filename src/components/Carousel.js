3ecab9740e5e6e8dd9ca8a5380d0420a6807410a
TimLaxton01 <tlaxton@twigeducatio.com>
2022-02-03 09:44:14 +0000

    added images

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import {motion} from 'framer-motion';
 import {useRef, useEffect, useState} from 'react'
+import images from '../images'
 
 const Carousel = () => {
+    console.log(images);
   return (
-  <motion.h1 animate={{x: 250}}>POP</motion.h1>
-  )
-};
+      <div>
+      
+  <motion.div className="carousel">
+      <motion.div className="inner-carousel">
+
+      </motion.div>
+  </motion.div>
+  </div>
+  );
+}
 
 export default Carousel;
---
+++ src/images.js
@@ -0,0 +1,8 @@
+import image1 from './asset/1.jpg'
+import image2 from './asset/2.jpg'
+import image3 from './asset/3.jpg'
+import image4 from './asset/4.jpg'
+import image5 from './asset/5.jpg'
+import image6 from './asset/6.jpg'
+
+export default [image1, image2, image3, image4, image5, image5, image6]
